ae4eaf45bf697dd8879e0287548e617453a390fe
djokjulapfe <djordjemarjanovic998@gmail.com>
2019-12-04 16:17:50 +0100

    Changed N to b

--- types.vhd
+++ types.vhd
@@ -3,7 +3,8 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 package types is
-	constant N : integer := 16;
+	-- number of bytes per word
+	constant b : integer := 16;
 	type complex is record
 		r, i: integer;
 	end record;
