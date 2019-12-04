bf53eb0ea506cbb4af51869e79f46a2d13e0e605
djokjulapfe <djordjemarjanovic998@gmail.com>
2019-12-04 17:58:34 +0100

    Convert tabs to spaces

--- types.vhd
+++ types.vhd
@@ -3,10 +3,10 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 package types is
-	-- number of bytes per word
-	constant b : integer := 16;
-	type complex is record
-		r, i: integer;
-	end record;
-	type complex_vector is array (natural range <>) of complex;
+    -- number of bytes per word
+    constant b : integer := 16;
+    type complex is record
+        r, i : integer;
+    end record;
+    type complex_vector is array (natural range <>) of complex;
 end package types;
--- upv_fft.vhd
+++ upv_fft.vhd
@@ -3,19 +3,19 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity upv_fft is
-	port (
-		clk      : in  std_logic;
-		uart_in  : in  std_logic;
-		uart_out : out std_logic
-	);
+    port (
+        clk      : in  std_logic;
+        uart_in  : in  std_logic;
+        uart_out : out std_logic
+    );
 end entity upv_fft;
 
 architecture behav of upv_fft is
 begin
-	update : process (clk) is
-	begin
-		if rising_edge(clk) then
-			uart_out <= uart_in;
-		end if;
-	end process;
+    update : process (clk) is
+    begin
+        if rising_edge(clk) then
+            uart_out <= uart_in;
+        end if;
+    end process;
 end architecture behav;
