367e965d364c144f2163e3980b262b39036b2a8d
djokjulapfe <djordjemarjanovic998@gmail.com>
2019-12-05 23:28:54 +0100

    Removed random comments

--- simulation/modelsim/upv_fft_tb.vhd
+++ simulation/modelsim/upv_fft_tb.vhd
@@ -2,43 +2,27 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
------------------------------------------------------------
-
 entity upv_fft_tb is
-
 end entity upv_fft_tb;
-
------------------------------------------------------------
 
 architecture testbench of upv_fft_tb is
 
-    -- Testbench DUT generics
-
-
-    -- Testbench DUT ports
     signal clk      : std_logic;
     signal uart_in  : std_logic;
     signal uart_out : std_logic;
 
-    -- Other constants
-    constant clk_period : time := 10 ns; -- NS
+    constant clk_period : time := 10 ns;
 
 begin
-    -----------------------------------------------------------
-    -- Clocks
-    -----------------------------------------------------------
-    CLK_GEN : process
+    clk_gen : process
     begin
         clk <= '1';
         wait for clk_period / 2;
         clk <= '0';
         wait for clk_period / 2;
-    end process CLK_GEN;
+    end process clk_gen;
 
-    -----------------------------------------------------------
-    -- Testbench Stimulus
-    -----------------------------------------------------------
-
+    -- random sequence, should be removed
     RND : process
     begin
         wait for clk_period / 2;
@@ -47,10 +31,7 @@ begin
         uart_in <= '1';
         wait for clk_period * 5;
     end process RND;
-
-    -----------------------------------------------------------
-    -- Entity Under Test
-    -----------------------------------------------------------
+    
     DUT : entity work.upv_fft
         port map (
             clk      => clk,
