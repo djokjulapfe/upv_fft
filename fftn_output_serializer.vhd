95af492ff47c6ca550568de7c10d7d46b15cc8e5
djokjulapfe <djordjemarjanovic998@gmail.com>
2019-12-09 20:22:32 +0100

    Finished fftn_output_serializer

--- fftn_output_serializer.vhd
+++ fftn_output_serializer.vhd
@@ -15,7 +15,7 @@ entity fftn_output_serializer is
         fftn_out   : in  complex_vector(0 to n - 1);
         start      : in  std_logic;
         step       : in  std_logic;
-        data       : out std_logic_vector(7 downto 0);
+        data       : out std_logic_vector(bits_per_data - 1 downto 0);
         data_ready : out std_logic
     );
 end entity fftn_output_serializer;
@@ -24,7 +24,7 @@ architecture behav of fftn_output_serializer is
 
     component counter is
         generic (
-            n : integer := n * 4
+            n : integer := 0
         );
         port (
             clk      : in     std_logic;
@@ -35,26 +35,79 @@ architecture behav of fftn_output_serializer is
         );
     end component counter;
 
-    constant datas_per_word = word_size / bits_per_data;
+    constant num_chunks : integer := word_size / bits_per_data;
 
-    signal selected_value : integer range 0 to n;
+    signal started : std_logic;
+
+    signal chunk_index   : integer range 0 to num_chunks * 2;
+    signal value_index   : integer range 0 to n;
+    signal item_finished : std_logic;
+    signal all_finished  : std_logic;
+
+    signal selected_value : complex;
+    signal selected_item  : signed(word_size - 1 downto 0);
+
+    signal item_step_mask  : std_logic;
+    signal chunk_step_mask : std_logic;
 
 begin
 
-    selected_value <= counter_value >> 2;
+    update_state : process (clk) is
+    begin
+        if rising_edge(clk) then
+            if reset = '1' then
+                started <= '0';
+            elsif start = '1' then
+                started <= '1';
+            elsif all_finished = '1' then
+                started <= '0';
+            end if;
+        end if;
+    end process update_state;
 
-    data <= std_logic_vector(fftn_out(counter_value));
+    selection : process (fftn_out, value_index, chunk_index) is
+        variable selection : signed(bits_per_data - 1 downto 0);
+    begin
+        selected_value <= fftn_out(value_index);
+        if chunk_index < num_chunks then
+            selected_item <= selected_value.re;
 
-    counter_1 : entity work.counter
+            selection := selected_item((chunk_index + 1) * bits_per_data - 1
+                    downto chunk_index * bits_per_data);
+        else
+            selected_item <= selected_value.im;
+
+            selection := selected_item((chunk_index - num_chunks + 1) * bits_per_data - 1
+                    downto (chunk_index - num_chunks) * bits_per_data);
+        end if;
+        data <= std_logic_vector(selection);
+    end process selection;
+
+    item_step_mask  <= item_finished and started;
+    chunk_step_mask <= step and started;
+
+    item_counter : entity work.counter
         generic map (
             n => n
         )
         port map (
             clk      => clk,
             reset    => reset,
-            step     => step,
-            value    => counter_value,
-            finished => counter_finished
+            step     => item_step_mask,
+            value    => value_index,
+            finished => all_finished
+        );
+
+    chunk_counter : entity work.counter
+        generic map (
+            n => num_chunks * 2
+        )
+        port map (
+            clk      => clk,
+            reset    => reset,
+            step     => chunk_step_mask,
+            value    => chunk_index,
+            finished => item_finished
         );
 
 end architecture behav;
---
+++ simulation/modelsim/fftn_output_serializer_tb.vhd
@@ -0,0 +1,80 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+use work.types.all;
+
+entity fftn_output_serializer_tb is
+end entity fftn_output_serializer_tb;
+
+architecture testbench of fftn_output_serializer_tb is
+
+	constant n             : integer := 16;
+	constant bits_per_data : integer := 8;
+
+	signal clk        : std_logic;
+	signal reset      : std_logic;
+	signal fftn_out   : complex_vector(0 to n - 1);
+	signal start      : std_logic;
+	signal step       : std_logic;
+	signal data       : std_logic_vector(bits_per_data - 1 downto 0);
+	signal data_ready : std_logic;
+
+	constant clk_period : time := 10 ns;
+
+begin
+
+	clk_gen : process is
+	begin
+		clk <= '0';
+		wait for clk_period / 2;
+		clk <= '1';
+		wait for clk_period / 2;
+	end process clk_gen;
+
+	main : process is
+	begin
+		-- init
+		reset <= '1';
+		step  <= '0';
+		start <= '0';
+		for i in 0 to n - 1 loop
+			fftn_out(i) <= pair2complex(i, 2 * i);
+		end loop;
+
+		wait for clk_period;
+
+		reset <= '0';
+		wait for clk_period * 5;
+		start <= '1';
+		wait for clk_period;
+		start <= '0';
+		wait for clk_period * 5;
+
+		for i in 0 to 16 * 5 loop
+			step <= '1';
+			wait for clk_period;
+			step <= '0';
+			wait for clk_period * 5;
+		end loop;
+
+		wait;
+	end process main;
+
+
+	DUT : entity work.fftn_output_serializer
+		generic map (
+			n             => n,
+			bits_per_data => bits_per_data
+		)
+		port map (
+			clk        => clk,
+			reset      => reset,
+			fftn_out   => fftn_out,
+			start      => start,
+			step       => step,
+			data       => data,
+			data_ready => data_ready
+		);
+
+end architecture testbench;
